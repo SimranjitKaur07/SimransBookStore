556533e9b186fd409b7f9f37346c9d3b5f4c42a8
SK627@myscc.ca <SK627@myscc.ca>
2022-03-29 19:44:03 -0400

    Add CRUD method in repo

--- SimransBooks.DataAccess/Repository/IRepository.cs
+++ SimransBooks.DataAccess/Repository/IRepository.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SimransBooks.DataAccess.Repository
+namespace SimransBooks.DataAccess.Repository.IRepository
 {
-    class IRepository
+    public interface IRepository<T> where T : class
     {
-    }
+        T Get(int id);
+
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null
+            );
+        void Add(T entity); // to add an entity
+        void Remove(int id); // to remove an object or category
+       void Remove(T entity); // another way to remove
+      void RemoveRange(IEnumerable<T> entity);  //remove a complete range of entities
+}
 }
