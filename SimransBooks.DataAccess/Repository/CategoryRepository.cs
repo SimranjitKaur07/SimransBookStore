c7d2062f6642556c3714882ed4a8ff91635384c4
SK627@myscc.ca <SK627@myscc.ca>
2022-03-29 20:18:51 -0400

    Category

---
+++ SimransBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using SimransBooks.DataAccess.Repository.IRepository;
+using SimransBooks.Models;
+using SimransBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimransBooks.DataAccess.Repository
+{
+   public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
