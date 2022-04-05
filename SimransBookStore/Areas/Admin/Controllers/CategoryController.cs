46f6e83ae08329f1954886e15b1f87c885558dc0
SK627@myscc.ca <SK627@myscc.ca>
2022-04-05 10:28:38 -0400

    category controller/ main proj

--- SimransBookStore/Areas/Admin/Controllers/CategoryController.cs
+++ SimransBookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -9,9 +9,23 @@ namespace SimransBookStore.Areas.Admin.Controllers
     [Area("Admin")]
     public class CategoryController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
             return View();
         }
+        //API calls here 
+        #region API CALLS
+        public IActionResult GetAll()
+        {
+            // return not found();
+            var allObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = allObj });
+        }
+        #endregion
     }
 }
